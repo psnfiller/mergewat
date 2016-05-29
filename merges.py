56b2c03c2eca739a0a92b2cc28ba0468e6924aae
Peter Nuttall <psn@nuttall.im>
2016-05-29 13:35:21 +0100

    checkpoint

--- merges.py
+++ merges.py
@@ -20,18 +20,56 @@ def smallest_merge(stack):
 def merge(stack):
   """Merges.
 
+  >>> merge([1,1])
+  ([1, 1], 0, 0, False)
   >>> merge([1,1,1])
-  ([3], 3, False)
+  ([1, 2], 2, 1, False)
+  >>> merge([1,1,1,1,1,1])
+  ([1, 5], 5, 1, True)
 
   """
+  max_depth = 5
+  merge_point = sys.maxint
+  depth = False
+  if len(stack) > 5:
+    depth = True
+    merge_point = 5
+
+  size = 0
+  for i in xrange(len(stack)):
+    if stack[i] <= size:
+      if merge_point > i:
+        merge_point = i
+    size += stack[i]
+
+  if merge_point == sys.maxint or merge_point +1  == len(stack):
+    return stack, 0, 0, False
+
+  merge_size = 0
+  for i in xrange(merge_point, len(stack)):
+    merge_size += stack[i]
+  output = stack[:merge_point] + [merge_size]
+  assert len(output) < max_depth
+  assert sum(stack) == sum(output), output
+  return output, merge_size, merge_point, depth
+
+def merge2(stack):
+  """Merges.
+
+
+  """
+  max_depth = 5
   merge_point = -1
   merge_size = 0
   depth = False
   if len(stack) > 5:
     depth = True
     merge_point = 5
-    for i in xrange(5):
+    for i in xrange(5, len(stack)):
       merge_size += stack[i]
+    output = stack[:merge_point] + [merge_size]
+    print output
+    assert sum(stack) == sum(output), output
   smallest = sys.maxint
   size = 0
   for i in xrange(len(stack)):
@@ -42,7 +80,10 @@ def merge(stack):
     size += stack[i]
   if merge_point == -1:
     return stack, 0, False
-  return [merge_size] + stack[merge_point+1:], merge_size, depth
+  output = [merge_size] + stack[merge_point:]
+  assert len(output) < max_depth
+  assert sum(stack) == sum(output), output
+  return output, merge_size, depth
 
 
 def run(total, minor_size):
@@ -51,7 +92,7 @@ def run(total, minor_size):
   for i in xrange(total / minor_size):
     stack = minor(stack, minor_size)
     #print stack
-    stack,s, depth  = merge(stack)
+    stack,s, _, depth  = merge(stack)
     tally += s
     print "merge", stack,  s, tally, depth
   return tally
@@ -69,8 +110,8 @@ def main():
 
 
 
-if __name__ == "__main__":
-  doctest.testmod()
-
 #if __name__ == "__main__":
-#  main()
+#  doctest.testmod()
+
+if __name__ == "__main__":
+  main()
